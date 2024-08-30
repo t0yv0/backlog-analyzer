86646b305254535e99ed603e781004e5b3072f98
Anton Tayanovskyy <anton@pulumi.com>
2024-08-30 17:53:08 -0400

    Experimenting

--- backloganalyzer/summarize.py
+++ backloganalyzer/summarize.py
@@ -1,12 +1,19 @@
 import ollama
+import random
 
 
 def summarize_issues(issues):
     stuff = issues
+    random.shuffle(stuff)
+
+    c = f'Please group these issues into related clusters, and for the largest clusters print the summary of themes and the number of issues in them: <issues>{stuff}</issues>'
+
+    print("PROMPT LENGTH", len(c))
+
     response = ollama.chat(model='mistral', messages=[
         {
             'role': 'user',
-            'content': f'Please group these issues into related clusters, and for the largest clusters print the summary of themes and the number of issues in them: <issues>{stuff}</issues>',
+            'content': c,
         },
     ])
 
